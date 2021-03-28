e0326923041749775e52e9ec31b95efacd1838ed
sudheendrakatikar <sudhu.katikar@gmail.com>
2021-03-28 23:49:07 +0530

    Williams %R

--- snippets/willr.py
+++ snippets/willr.py
@@ -1,9 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Mar 28 16:33:12 2021
-
-@author: blackmagic
+Williams %R = HighestHigh(t) - Close / HighestHigh(t) - LowestLow(t)
+Range    Signal
+0-20     SELL
+80-100   BUY
 """
 
 import yfinance as yf
-import numpy as np
+from datetime import datetime, timedelta
+
+stock = 'TSLA'
+end = datetime.today().strftime('%Y-%m-%d')
+start = (datetime.today() - timedelta(360)).strftime('%Y-%m-%d')
+t = 14
+
+data = yf.download(stock, start, end)
+data['nr'] = data['High'].rolling(t).max() - data['Close']
+data['dr'] = data['High'].rolling(t).max() - data['Low'].rolling(t).min()
+data['wr'] = data['nr'] / data['dr'] * 100
+
+ax = data['wr'].plot(title='Williams %R: '+stock, ylim=(0,100))
+ax.axhline(y=80, color='red', ls=':')
+ax.axhline(y=20, color='green', ls=':')
